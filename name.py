9890a829e2da7e0240b149a2199b983e71b7e5fd
anna <Ann.Badamshina@gmail.com>
2025-03-12 20:42:05 +0700

    spiral6

---
+++ name.py
@@ -0,0 +1,7 @@
+my_name = "аня"
+my_age = 12
+your_name = input("Как тебя зовут? ")
+your_age = input("Сколько тебе лет? ")
+print("меня зовут", my_name,", и мне" , my_age ,"лет.")
+print("тебя зовут", your_name, ", и тебе", your_age, ".")
+print("Спасибо за покупку моей книги,", your_name, "!")
