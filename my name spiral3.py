dbf44ca69d34446dc94be76786e22140f9892c75
anna <Ann.Badamshina@gmail.com>
2025-05-08 15:38:28 +0700

    my name5

---
+++ my name spiral3.py
@@ -0,0 +1,8 @@
+name = input("как тебя зовут?")
+while name!= "":
+    for x in range (5) :
+        print(name, end = " ")
+    print ()
+
+    name = input("ввудите еще имя или нажмите [Enter], чтобы выйти:")
+print("Спасибо за игру!")
